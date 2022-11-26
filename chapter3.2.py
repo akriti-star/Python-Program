9239cfc29f09187d94c0f7c4d1a149c8dbdcfdc4
akriti-star <sharmaakriti8678@gmail.com>
2022-11-26 22:27:18 +0530

    practice

---
+++ chapter3.2.py
@@ -0,0 +1,6 @@
+user_name=input("enter the name")
+user_age=int(input("enter the age"))
+if user_age>=10 and (user_name[0]=='a'or user_name[0]=='A'):
+    print("YOU CAN WATCH COCO MOVIE")
+else:
+    print("YOU CANNOT WATCH  COCO MOVIE")
